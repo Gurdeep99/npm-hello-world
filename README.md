13e029950ab9e694e6b6e44289755166afce5528
Gurdeep <gurdeep.singh@auw.co.in>
2024-09-09 10:15:09 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ##Npm Test 
+
+convertCurrencies("USD", "INR", 100).then(res=> console.log(res));
---
+++ index.js
@@ -0,0 +1,14 @@
+import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+
+const freecurrencyapi = new Freecurrencyapi();
+ 
+export default async function convertCurrencies(from, to, amount){
+
+ const responce = await freecurrencyapi.latest({
+    base_currency: from,
+    currencies: to,
+});
+    const amount_currency = responce.data[to] * amount;
+  return amount_currency ;
+
+}
